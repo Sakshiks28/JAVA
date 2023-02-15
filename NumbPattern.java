0e52b792de3b04c5a5ae624034d574d7184961eb
Sakshiks28 <123305244+Sakshiks28@users.noreply.github.com>
2023-02-15 15:41:47 +0530

    Add files via upload
    Assignment- Same Number Pattern

---
+++ NumbPattern.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class NumbPattern
+{
+    public static void main(String args[])
+    {
+        int a,b;
+
+        for(a=1; a<=5;a++)
+        {
+            for(b=1; b<=a; b++)
+            {
+                System.out.print(a);
+            }
+            System.out.println();
+        }
+    }
+}
+
